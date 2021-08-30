5f3e41c935562a59ca7af6a2cea43629d42e6ab1
root <root@debian.debian>
2021-08-30 09:07:53 -0400

    adding nginx to docker-compose

--- django/entrypoint.sh
+++ django/entrypoint.sh
@@ -5,7 +5,7 @@ python manage.py makemigrations
 python manage.py migrate 
 
 #python manage.py migrate --no-input
-#python manage.py collectstatic --no-input
-python manage.py runserver 0.0.0.0:8000
-#gunicorn project.wsgi:application --bind 0.0.0.0:8000
+python manage.py collectstatic
+#python manage.py runserver 0.0.0.0:8000
+gunicorn wsgi:application --bind 0.0.0.0:8000
 
